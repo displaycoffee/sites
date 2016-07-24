4e309a54fdd028a1a760de7a87cfa1c50219dcb0
displaycoffee <adria.m.murphy@gmail.com>
2016-07-24 16:17:01 -0600

    Create home page and add navigation styles

---
+++ themusescircle/dev/themes/themusescircle/front-page.php
@@ -0,0 +1,26 @@
+<?php
+	/**
+	* Template for displaying the front page
+	*
+	* Settings > Reading > Static page > Front page
+	*/
+
+	// Exit if accessed directly
+	if ( !defined( 'ABSPATH' ) ) { exit; }
+	
+	// Include header	
+	get_header(); 
+?>
+<section id="home-slider">
+	<div class="wrapper">stuff</div>
+</section>
+<section id="home-latest-reviews">
+	<div class="wrapper">stuff</div>	
+</section>
+<section id="home-more-reviews">
+	<div class="wrapper">stuff</div>	
+</section>
+<section id="home-blog-roll">
+	<div class="wrapper">stuff</div>	
+</section>
+<?php get_footer(); ?>
--- themusescircle/wp-content/themes/themusescircle/header.php
+++ themusescircle/wp-content/themes/themusescircle/header.php
@@ -22,13 +22,18 @@
 			<?php _e( 'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.', 'themusescircle' ); ?>
 		</p>
 	<![endif]-->
+	<nav id="header-nav" class="navigation">
+		<div class="wrapper cf">
+			<div class="search-button">
+				<a><i class="fa fa-search" aria-hidden="true"></i>Search</a>
+			</div>
+			<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+		</div>
+	</nav>
 	<header id="header">
 		<p class="site-name">
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
 		</p>
 		<p class="site-description"><?php echo get_bloginfo( 'description' ); ?></p>
 	</header>
-	<nav id="header-nav" class="navigation">
-		<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-	</nav>
 	<?php get_search_form(); ?>		
