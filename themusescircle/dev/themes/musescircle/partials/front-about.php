212c29db1ad70dd80c985fb43d27f8c924aa18be
displaycoffee <adria.m.murphy@gmail.com>
2016-08-27 16:00:59 -0600

    Clean up front page and create partial files for each section

---
+++ themusescircle/dev/themes/musescircle/partials/front-about.php
@@ -0,0 +1,48 @@
+<?php
+	/**
+	* Template for displaying front page "About" section
+	*/
+
+	// Exit if accessed directly
+	if ( !defined( 'ABSPATH' ) ) { exit; }
+
+	// Grab all the variables we need for this page	
+	$about_hide = get_theme_mod( 'musescircle_about_hide' );
+	$about_title = get_theme_mod( 'musescircle_about_title' );
+	$about_left = get_theme_mod( 'musescircle_about_left' );
+	$about_right = get_theme_mod( 'musescircle_about_right' );
+	$about_more_text = get_theme_mod( 'musescircle_about_more_text' );
+	$about_more_url = get_theme_mod( 'musescircle_about_more_url' );	
+?>
+<?php 
+	// Default text for left column
+	$about_default_text = '<p>Welcome to our site! We\'re just getting started, but be sure to check back for more information soon.</p>';
+
+	// Create "About" block - START
+	$about_section = '<section id="about-site"><div class="wrapper">';
+	$about_section .= '<h2>';
+	$about_section .= $about_title ? esc_html( $about_title ) : __( 'About', 'musescircle' );
+	$about_section .= '</h2>';
+	$about_section .= '<div class="row"><div class="column">';
+	$about_section .= $about_left ? wpautop( esc_textarea ( $about_left ) ) : __( $about_default_text, 'musescircle' );
+	$about_section .= '</div>';
+
+	// Check if the right column has content
+	if ( $about_right ) {
+		$about_section .= '<div class="column">';
+		$about_section .= wpautop( esc_textarea ( $about_right ) );
+		$about_section .= '</div>';
+	}				
+
+	// Create "About" block - END
+	$about_section .= '</div><div class="read-more">';
+	$about_section .= '<a href="' . ( $about_more_url ? esc_url( $about_more_url ) : esc_url ( get_bloginfo( 'url' ) . '/about' ) ) . '">';
+	$about_section .= $about_more_text ? esc_html( $about_more_text ) : __( 'Read More', 'musescircle' );
+	$about_section .= '</a></div>';		
+	$about_section .= '</div></section>';
+
+	// Display "About" section (only if not marked as hidden)
+	if ( !$about_hide ) {
+		echo $about_section;
+	}
+?>
---
+++ themusescircle/dev/themes/musescircle/partials/front-review-buttons.php
@@ -0,0 +1,63 @@
+<?php
+	/**
+	* Template for displaying front page "Review Buttons" section
+	*/
+
+	// Exit if accessed directly
+	if ( !defined( 'ABSPATH' ) ) { exit; }
+
+	// Grab all the variables we need for this page	
+	$review_buttons_hide = get_theme_mod( 'musescircle_review_buttons_hide' );
+	$review_buttons_title = get_theme_mod( 'musescircle_review_buttons_title' );
+	$review_buttons_book_url = get_theme_mod( 'musescircle_review_buttons_book_url' );
+	$review_buttons_hide_books = get_theme_mod( 'musescircle_review_buttons_hide_books' );
+	$review_buttons_movie_url = get_theme_mod( 'musescircle_review_buttons_movie_url' );
+	$review_buttons_hide_movies = get_theme_mod( 'musescircle_review_buttons_hide_movies' );
+	$review_buttons_tv_url = get_theme_mod( 'musescircle_review_buttons_tv_url' );
+	$review_buttons_hide_tv = get_theme_mod( 'musescircle_review_buttons_hide_tv' );
+	$review_buttons_everything_url = get_theme_mod( 'musescircle_review_buttons_everything_url' );
+	$review_buttons_hide_everything = get_theme_mod( 'musescircle_review_buttons_hide_everything' );	
+?>
+<?php 
+	// Create "Review Buttons" block - START
+	$review_buttons_section = '<section id="review-buttons"><div class="wrapper">';
+	$review_buttons_section .= '<h3>';
+	$review_buttons_section .= $review_buttons_title ? esc_html( $review_buttons_title ) : __( 'Looking for more reviews?', 'musescircle' );
+	$review_buttons_section .= '</h3>';
+
+	// Check if book review button is not hidden
+	if ( !$review_buttons_hide_books ) {
+		$review_buttons_section .= '<a href="' . ( $review_buttons_book_url ? esc_url( $review_buttons_book_url ) : esc_url ( get_bloginfo( 'url' ) . '/insprvw-book-review' ) ) . '" class="book button-simple">';
+		$review_buttons_section .= __( 'Books', 'musescircle' );
+		$review_buttons_section .= '</a>';	
+	}
+
+	// Check if movie review button is not hidden
+	if ( !$review_buttons_hide_movies ) {
+		$review_buttons_section .= '<a href="' . ( $review_buttons_movie_url ? esc_url( $review_buttons_movie_url ) : esc_url ( get_bloginfo( 'url' ) . '/insprvw-movie-review' ) ) . '" class="movie button-simple">';
+		$review_buttons_section .= __( 'Movies', 'musescircle' );
+		$review_buttons_section .= '</a>';	
+	}
+
+	// Check if tv review button is not hidden
+	if ( !$review_buttons_hide_tv ) {
+		$review_buttons_section .= '<a href="' . ( $review_buttons_tv_url ? esc_url( $review_buttons_tv_url ) : esc_url ( get_bloginfo( 'url' ) . '/insprvw-tv-review' ) ) . '" class="tv button-simple">';
+		$review_buttons_section .= __( 'TV', 'musescircle' );
+		$review_buttons_section .= '</a>';	
+	}
+
+	// Check if everything review button is not hidden
+	if ( !$review_buttons_hide_everything ) {
+		$review_buttons_section .= '<a href="' . ( $review_buttons_everything_url ? esc_url( $review_buttons_everything_url ) : esc_url ( get_bloginfo( 'url' ) . '/all-reviews' ) ) . '" class="everything button-simple">';
+		$review_buttons_section .= __( 'Everything', 'musescircle' );
+		$review_buttons_section .= '</a>';	
+	}						
+
+	// Create "Review Buttons" block - END
+	$review_buttons_section .= '</div></section>';
+
+	// Display "Review Buttons" section (only if not marked as hidden)
+	if ( !$review_buttons_hide ) {
+		echo $review_buttons_section;
+	}
+?>
