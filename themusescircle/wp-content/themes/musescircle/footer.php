3de901f17d11f15abe5b879c9ee8177c1e4df923
displaycoffee <adria.m.murphy@gmail.com>
2016-09-10 18:13:56 -0600

    Updates to header and footer elements

--- themusescircle/wp-content/themes/musescircle/footer.php
+++ themusescircle/wp-content/themes/musescircle/footer.php
@@ -9,15 +9,17 @@
 	<?php if ( is_active_sidebar( 'footer-column01' ) || is_active_sidebar( 'footer-column02' ) || is_active_sidebar( 'footer-column03' ) ) : ?>
 		<footer id="footer">
 			<div class="wrapper">
-				<?php if ( is_active_sidebar( 'footer-column01' ) ) : ?>
-					<?php dynamic_sidebar( 'footer-column01' ); ?>
-				<?php endif; ?>
-				<?php if ( is_active_sidebar( 'footer-column02' ) ) : ?>
-					<?php dynamic_sidebar( 'footer-column02' ); ?>
-				<?php endif; ?>
-				<?php if ( is_active_sidebar( 'footer-column03' ) ) : ?>
-					<?php dynamic_sidebar( 'footer-column03' ); ?>
-				<?php endif; ?>
+				<div class="row">
+					<?php if ( is_active_sidebar( 'footer-column01' ) ) : ?>
+						<?php dynamic_sidebar( 'footer-column01' ); ?>
+					<?php endif; ?>
+					<?php if ( is_active_sidebar( 'footer-column02' ) ) : ?>
+						<?php dynamic_sidebar( 'footer-column02' ); ?>
+					<?php endif; ?>
+					<?php if ( is_active_sidebar( 'footer-column03' ) ) : ?>
+						<?php dynamic_sidebar( 'footer-column03' ); ?>
+					<?php endif; ?>
+				</div>
 			</div>
 		</footer>
 	<?php endif; ?>	
