4f338cc9709893ac93e52d2c8bb71b9339e13557
displaycoffee <adria.m.murphy@gmail.com>
2016-09-11 20:19:21 -0600

    Update footer widgets

--- themusescircle/wp-content/themes/musescircle/footer.php
+++ themusescircle/wp-content/themes/musescircle/footer.php
@@ -11,13 +11,19 @@
 			<div class="wrapper">
 				<div class="row">
 					<?php if ( is_active_sidebar( 'footer-column01' ) ) : ?>
-						<?php dynamic_sidebar( 'footer-column01' ); ?>
+						<div id="footer-column01" class="column">
+							<?php dynamic_sidebar( 'footer-column01' ); ?>
+						</div>
 					<?php endif; ?>
 					<?php if ( is_active_sidebar( 'footer-column02' ) ) : ?>
-						<?php dynamic_sidebar( 'footer-column02' ); ?>
+						<div id="footer-column02" class="column">
+							<?php dynamic_sidebar( 'footer-column02' ); ?>
+						</div>
 					<?php endif; ?>
 					<?php if ( is_active_sidebar( 'footer-column03' ) ) : ?>
-						<?php dynamic_sidebar( 'footer-column03' ); ?>
+						<div id="footer-column03" class="column">
+							<?php dynamic_sidebar( 'footer-column03' ); ?>
+						</div>
 					<?php endif; ?>
 				</div>
 			</div>
