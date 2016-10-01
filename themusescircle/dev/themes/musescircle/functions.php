733d94047254bd28723a598ed1ef26e617e8062c
displaycoffee <adria.m.murphy@gmail.com>
2016-10-01 12:49:09 -0600

    Style comment list

--- themusescircle/dev/themes/musescircle/functions.php
+++ themusescircle/dev/themes/musescircle/functions.php
@@ -110,35 +110,37 @@
 	    }
     ?>
     	<li <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?> id="comment-<?php esc_attr( comment_ID() ) ?>">
-	    	<div class="comment-meta">
-		    	<p class="author"><?php printf( __( 'Comment by %s', 'musescircle' ), get_comment_author_link() ); ?></p>
-			    <p class="date"><?php printf( __( 'on %1$s at %2$s', 'musescircle' ), get_comment_date(), get_comment_time() ); ?></p>
-			</div>
 	        <?php 
 	        	if ( $args['avatar_size'] != 0 ) {
-	        		echo '<div class="comment-thumbnail"><div class="image-wrap">' . get_avatar( $comment, $args['avatar_size'] ) . '</div></div>';
+	        		echo '<div class="comment-thumbnail"><div class="image-wrap">' . get_avatar( $comment, 50 ) . '</div></div>';
 	        	} 
 	        ?>
-			<div class="comment-content">
-				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<p class="moderation"><em><?php _e( 'Your comment is awaiting moderation.', 'musescircle' ); ?></em></p>
-				<?php endif; ?>
-				<?php comment_text(); ?>
-			</div>
-    		<footer class="comment-footer">
-    			<?php edit_comment_link( __( 'Edit', 'musescircle' ), '<div class="edit">', '</div>' ); ?>
-    			<?php 
-    				comment_reply_link( 
-    					array_merge( $args, array( 
-    						'add_below' => $add_below, 
-    						'depth'		=> $depth, 
-    						'max_depth' => $args['max_depth'],
-    						'before'	=> '<div class="reply">',
-    						'after'		=> '</div>' 
-    					) ) 
-    				); 
-    			?>
-    		</footer>
+	        <div class="comment-wrapper">
+		    	<div class="comment-meta">
+			    	<p class="author"><?php printf( __( 'Comment by %s', 'musescircle' ), get_comment_author_link() ); ?></p>
+				    <p class="date"><?php printf( __( 'on %1$s at %2$s', 'musescircle' ), get_comment_date(), get_comment_time() ); ?></p>
+				</div>
+				<div class="comment-content">
+					<?php if ( $comment->comment_approved == '0' ) : ?>
+						<p class="moderation"><em><?php _e( 'Your comment is awaiting moderation.', 'musescircle' ); ?></em></p>
+					<?php endif; ?>
+					<?php comment_text(); ?>
+				</div>
+	    		<footer class="comment-footer">
+	    			<?php edit_comment_link( __( 'Edit', 'musescircle' ), '<p class="edit">', '</p><span class="bullet">&bull;</span>' ); ?>
+	    			<?php 
+	    				comment_reply_link( 
+	    					array_merge( $args, array( 
+	    						'add_below' => $add_below, 
+	    						'depth'		=> $depth, 
+	    						'max_depth' => $args['max_depth'],
+	    						'before'	=> '<p class="reply">',
+	    						'after'		=> '</p>' 
+	    					) ) 
+	    				); 
+	    			?>
+	    		</footer>
+	    	</div>
     <?php
 	}
 
