9a360beb6c159b905827a967f8f4f66e74d49642
displaycoffee <adria.m.murphy@gmail.com>
2016-11-12 12:04:38 -0700

    Add social sharing buttons to reviews

--- themusescircle/dev/plugins/inspire-reviews/display/partials/review-footer.php
+++ themusescircle/dev/plugins/inspire-reviews/display/partials/review-footer.php
@@ -14,6 +14,9 @@
 		// Display list of tags
 		echo get_the_term_list( $post->ID, 'insprvw-' . insprvw_review_type( false ) . '-tag', '<p class="tags" itemprop="keywords"><strong>' . __( 'Tags', 'inspire-reviews' ) . ':</strong> ', ', ', '</p>' );
 
+		// sharing buttons
+		include INSPRVW_DIR . 'display/partials/sharing-buttons.php';
+
 		// Display edit post link
 		edit_post_link( __( 'Edit Content', 'inspire-reviews' ), '<p class="edit">', '</p>' );
 	?>
