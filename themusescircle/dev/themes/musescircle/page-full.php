3e2b705ae7411fe990981c5dce85f04e7c4283cd
displaycoffee <adria.m.murphy@gmail.com>
2016-12-16 18:26:11 -0700

    Add full width page template

---
+++ themusescircle/dev/themes/musescircle/page-full.php
@@ -0,0 +1,29 @@
+<?php
+	/**
+	* Template Name: Full Width Page
+	* Template for displaying full width pages
+	*/
+
+	// Exit if accessed directly
+	if ( !defined( 'ABSPATH' ) ) { exit; }
+
+	// Include header	
+	get_header(); 
+?>
+<?php get_template_part( 'page', 'title' ); ?>
+<section class="content">
+	<div class="wrapper">
+		<article>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="entry-single">	
+					<div id="entry-<?php esc_attr( the_ID() ); ?>" class="entry page">
+						<?php get_template_part( 'partials/entry', 'thumbnail' ); ?>
+						<div class="entry-content"><?php the_content(); ?></div>
+						<?php edit_post_link( __( 'Edit Content', 'musescircle' ), '<p class="edit">', '</p>' ); ?>							
+					</div>
+				</div>
+			<?php endwhile; wp_reset_postdata(); endif; ?>
+		</article>
+	</div>
+</section>			
+<?php get_footer(); ?>
