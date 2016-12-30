7d940b21200b3555643081e2e1309299a7e366f3
displaycoffee <adria.m.murphy@gmail.com>
2016-12-29 20:30:29 -0700

    Fixes for responsive header

--- themusescircle/dev/plugins/inspire-reviews/display/partials/review-title.php
+++ themusescircle/dev/plugins/inspire-reviews/display/partials/review-title.php
@@ -8,22 +8,24 @@
 ?>
 <header id="header" class="main-header">
 	<div class="wrapper">
-		<h1>
-			<?php 				
-				if ( is_archive() ) {
-					// Check if on acrhive
-					if ( is_author() ) {
-						// Check if the archive is an author archive
-						_e( 'Author: ', 'inspire-reviews' );
-						echo get_the_author();
+		<div class="header-content">
+			<h1>
+				<?php 				
+					if ( is_archive() ) {
+						// Check if on acrhive
+						if ( is_author() ) {
+							// Check if the archive is an author archive
+							_e( 'Author: ', 'inspire-reviews' );
+							echo get_the_author();
+						} else {
+							echo get_the_archive_title();
+						}
 					} else {
-						echo get_the_archive_title();
+						// For everything else
+						echo get_the_title();
 					}
-				} else {
-					// For everything else
-					echo get_the_title();
-				}
-			?>		
-		</h1>
+				?>		
+			</h1>
+		</div>
 	</div>
 </header>
--- themusescircle/dev/themes/musescircle/page-title.php
+++ themusescircle/dev/themes/musescircle/page-title.php
@@ -15,35 +15,37 @@
 ?>
 <header id="header" class="main-header">
 	<div class="wrapper">
-		<h1 class="<?php echo $thumbnail_class; ?>">
-			<?php 				
-				if ( is_404() ) {
-					// Check if on 404 page
-					_e( 'Not Found', 'musescircle' );
-				} else if ( is_archive() ) {
-					// Check if on acrhive
-					if ( is_author() ) {
-						// Check if the archive is an author archive
-						_e( 'Author: ', 'musescircle' );
-						echo get_the_author();
+		<div class="header-content">
+			<h1 class="<?php echo $thumbnail_class; ?>">
+				<?php 				
+					if ( is_404() ) {
+						// Check if on 404 page
+						_e( 'Not Found', 'musescircle' );
+					} else if ( is_archive() ) {
+						// Check if on acrhive
+						if ( is_author() ) {
+							// Check if the archive is an author archive
+							_e( 'Author: ', 'musescircle' );
+							echo get_the_author();
+						} else {
+							echo get_the_archive_title();
+						}
+					} else if ( is_search() ) {
+						// Check if a search query is present
+						if ( get_search_query() ) {
+							echo sprintf( __( 'Search results for "%s"', 'musescircle' ), esc_html( get_search_query() ) );
+						} else {
+							_e( 'Search results', 'musescircle' );
+						}
+					} else if ( is_home() && !is_front_page() ) {
+						// Check if on blog				
+						echo single_post_title();
 					} else {
-						echo get_the_archive_title();
+						// For everything else
+						echo get_the_title();
 					}
-				} else if ( is_search() ) {
-					// Check if a search query is present
-					if ( get_search_query() ) {
-						echo sprintf( __( 'Search results for "%s"', 'musescircle' ), esc_html( get_search_query() ) );
-					} else {
-						_e( 'Search results', 'musescircle' );
-					}
-				} else if ( is_home() && !is_front_page() ) {
-					// Check if on blog				
-					echo single_post_title();
-				} else {
-					// For everything else
-					echo get_the_title();
-				}
-			?>		
-		</h1>
+				?>		
+			</h1>
+		</div>
 	</div>
 </header>
