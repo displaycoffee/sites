6ff69f155043ba708cb94fc04da2c6330240cad1
displaycoffee <adria.m.murphy@gmail.com>
2017-02-21 20:24:46 -0700

    Remove http on browsehappy link.

--- themusescircle/dev/themes/musescircle/header.php
+++ themusescircle/dev/themes/musescircle/header.php
@@ -19,7 +19,7 @@
 <body <?php body_class(); ?>>
 	<!--[if lte IE 9]>
 		<p class="browserupgrade">
-			<?php _e( 'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.', 'musescircle' ); ?>
+			<?php _e( 'You are using an <strong>outdated</strong> browser. Please <a href="//browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.', 'musescircle' ); ?>
 		</p>
 	<![endif]-->	
 	<?php 
