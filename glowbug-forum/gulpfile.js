959d4ca9d5ad11f9642ee119c082e80d36be5bb6
displaycoffee <adria.m.murphy@gmail.com>
2017-04-21 22:41:09 -0600

    Set up custom glowbug child theme with gulpfile

---
+++ glowbug-forum/gulpfile.js
@@ -0,0 +1,103 @@
+/* Required Sources
+   ---------------------------------------------- */
+
+var browserSync = require('browser-sync').create(),
+	gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	concat = require('gulp-concat'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify');
+
+/* Global Variables
+   ---------------------------------------------- */
+
+var proxyURL = 'http://localhost/sites/glowbug-forum';
+
+/* Development Variables
+   ---------------------------------------------- */
+
+var dev = '_dev/styles/glowbug_v1';
+var devJS = dev + '/template/js';
+var devSass = dev + '/theme/scss';
+
+/* Distribution Variables
+   ---------------------------------------------- */
+
+var dist = 'styles/glowbug_v1/';
+var distJS = dist + '/template/js';
+var distCSS = dist + '/theme';
+
+/* JavaScript
+   ---------------------------------------------- */
+var jsSources = [		
+	devJS + '/main.js',
+	devJS + '/run-functions.js'
+];
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('glowbug_v1.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(distJS));
+});
+
+/* SASS
+   ---------------------------------------------- */
+
+var sassSources = [
+	devSass + '/stylesheet.scss',
+	devSass + '/partials/**.scss'
+];
+
+gulp.task('sass', function() {
+	gulp.src(sassSources)
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({
+            browsers: ['last 2 versions', 'Explorer >= 10', 'Android >= 4.1', 'Safari >= 7', 'iOS >= 7']
+        }))
+		.pipe(gulp.dest(distCSS));
+});
+
+/* CSS
+   ---------------------------------------------- */
+
+var cssSources = [distCSS + '/**.css'];	
+
+gulp.task('css', function() {
+	gulp.src(cssSources)
+		.pipe(browserSync.stream());
+});
+
+/* Static Files
+   ---------------------------------------------- */
+
+var staticSources = [
+	dev + '/**/*.cfg',
+	dev + '/**/*.php',
+	dev + '/**/*.txt',
+	dev + '/**/images/*.*'
+];
+
+gulp.task('static', function() {
+	gulp.src(staticSources)
+		.pipe(gulp.dest(dist));
+});
+
+/* Watch All The Things
+   ---------------------------------------------- */
+
+gulp.task('watch', function() {
+    browserSync.init({
+        proxy: proxyURL,
+        open: false
+    })
+	gulp.watch(jsSources, ['js']);
+	gulp.watch(sassSources, ['sass']);
+	gulp.watch(cssSources, ['css']);
+	gulp.watch(staticSources, ['static']);
+});
+
+/* Default Gulp Task
+   ---------------------------------------------- */
+
+gulp.task('default', ['js', 'sass', 'css', 'static', 'watch']);
