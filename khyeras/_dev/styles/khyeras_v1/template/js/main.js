21fd4b8971320856bc3caf341efbbaea487b5297
displaycoffee <adria.m.murphy@gmail.com>
2017-06-14 21:59:12 -0600

    Initial commit of site style

---
+++ khyeras/_dev/styles/khyeras_v1/template/js/main.js
@@ -0,0 +1,416 @@
+// Toggle navigation sub menus
+function toggleNavSubMenus( selector ) {
+	jQuery( selector ).each( function() {
+		// Get current selector and check if there are any second level lists
+		var currentNav = jQuery( this );
+		var children = currentNav.children( 'ul' );
+
+		// If there is a second level list, add a menu toggle icon
+		if ( children.length > 0 ) {
+			// Add a toggle icon after main link element
+			jQuery( '<span class="icon icon-chevron-down toggle-submenu"></span>' ).insertAfter( currentNav.find( '> a' ) ); 
+
+			// Create sub menu variables for targetting			
+			var subMenuButton = currentNav.find( '.toggle-submenu' );
+			var subMenu = currentNav.find( '.sub-menu' );
+
+			// Toggle show/hide class if search button is clicked on
+			jQuery( subMenuButton ).click( function() {
+				jQuery( subMenu ).toggleClass( 'show' );
+				jQuery( subMenuButton ).toggleClass( 'show' );
+			});
+
+			// If anything outside search-button is clicked on, hide the search bar
+			jQuery( document ).on( 'click', function( event ) {
+				if ( !jQuery( event.target ).closest( currentNav ).length ) {
+					jQuery( subMenu ).removeClass( 'show' );
+					jQuery( subMenuButton ).removeClass( 'show' );
+				}
+			}); 
+		}
+	});
+}
+
+// Remove navigation when empty - mostly for attachment.php navigation
+function hideNavigation( selector ) {
+	var previous = jQuery( selector ).find( '.prev' );
+	var next = jQuery( selector ).find( '.next' );
+
+	// Remove previous link there's nothing there
+	if ( previous.children().length == 0 ) {
+		previous.remove();
+	}
+
+	// Remove next link there's nothing there
+	if ( next.children().length == 0 ) {
+		next.remove();
+	}
+
+	// If previous and next links are empty, remove pagination/selector container
+	if ( ( previous.children().length == 0 ) && ( next.children().length == 0 ) ) {
+		jQuery( selector ).remove();
+	}
+}
+
+// Convert WordPress galleries with images into a featherlight gallery
+function addFeatherLightGallery( selector ) {
+	jQuery( selector ).each( function() {
+		// Get variables for the galleries  
+		var currentGallery = jQuery( this );
+		var galleryID = currentGallery.attr( 'id' );
+		var galleryImageLink = currentGallery.find( '.gallery-item .gallery-icon a' );
+		var galleryValid;
+
+		// Check all links in a gallery to see if they link to valid image file extensions
+		jQuery( galleryImageLink ).each( function() {
+			// Get the current image link
+			var currentImageLink = jQuery( this );
+
+			// Check if the link has a valid image extension
+			var srcCheck = ( /\.(gif|jpg|jpeg|tiff|png|bmp|svg)$/i ).test( currentImageLink.attr( 'href' ) ); 
+
+			// If gallery has all valid links, set gallery to valid
+			if ( srcCheck == false ) {
+				galleryValid = false;
+				return false;
+			} else {
+				galleryValid = galleryID;
+			}
+		});
+
+		// If the gallery is valid...
+		if ( galleryValid == galleryID ) {
+			jQuery( galleryImageLink ).featherlightGallery({
+				previousIcon  : '<span class="icon icon-chevron-left"></span>',
+				nextIcon	  : '<span class="icon icon-chevron-right"></span>',
+				closeIcon	  : '<span class="icon icon-close"></span>',
+				galleryFadeIn : 300,
+				openSpeed	  : 300,
+				afterOpen	  : function(event){
+					jQuery( 'body, html' ).addClass( 'featherlight-open' );
+				},
+				afterClose	  : function(event){
+					jQuery( 'body, html' ).removeClass( 'featherlight-open' );
+				}										
+			});
+		}
+	});
+}
+
+// Check url function from http://stackoverflow.com/a/22519594
+function checkURL( url ) {
+	var regexURL = /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
+	if( !regexURL.test( url ) ) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+// Countdown functions modified from http://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies
+// Dates can be entered as... month/day/year, month-day-year, or Month Day, Year
+
+// Get remaining time from an end date
+function getRemainingTime( endTime ) {
+	// Get remaining time by subtracting end and current date
+	var remainingTime = Date.parse( new Date( endTime ) ) - Date.parse( new Date() );	
+
+	// Create total number of seconds for time calculations
+	var totalSeconds = Math.floor( remainingTime / 1000 );
+
+	// Create object to store time data
+	return {
+		'remaining' : remainingTime,
+		'days'	  : Math.floor( totalSeconds / 60 / 60 / 24 ),
+		'hours'	 : Math.floor( totalSeconds / 60 / 60 ) % 24,
+		'minutes'   : Math.floor( totalSeconds / 60 ) % 60,
+		'seconds'   : totalSeconds % 60
+	};
+}
+
+// Initialize countdown
+function initializeCountdown() {
+	// Loop through all countdown elements on the page
+	jQuery( '.countdown' ).each( function() {
+		// Countdown selector 
+		var countdown = jQuery( this );
+
+		// Get countdown data variables
+		var end = this.getAttribute('data-end-date');
+		var content = ( this.getAttribute('data-content') ? this.getAttribute('data-content').replace( /(<([^>]+)>)/ig, '' ) : '' );
+		var url = this.getAttribute('data-url');
+		var urlWindow = this.getAttribute('data-window');
+
+		// Get time values
+		var time = getRemainingTime( end );
+
+		// Check if there is any remaining time left
+		if ( time.remaining > 0 ) {
+			// Create inner countdown markup
+			function createCountdownMarkup( text ) {
+				return '<div class="' + text + '"><span class="countdown-value"></span><span class="countdown-label">' + text + '</span></div>'
+			}
+
+			// Add timer elements to countdown
+			countdown.append( '<div class="countdown-timer"></div>' );
+			countdown.find( '.countdown-timer' ).append( createCountdownMarkup( 'days' ), createCountdownMarkup( 'hours' ), createCountdownMarkup( 'minutes' ), createCountdownMarkup( 'seconds' ) );
+
+			// Selectors for time elements
+			var daysSelector = countdown.find( '.days .countdown-value' );
+			var hoursSelector = countdown.find( '.hours .countdown-value' );
+			var minutesSelector = countdown.find( '.minutes .countdown-value' );
+			var secondsSelector = countdown.find( '.seconds .countdown-value' );
+
+			// Make sure time is at least two digits
+			function formatTime( time ) {
+				if ( time > 9) {
+					return time;
+				} else {
+					return ( '0' + time ).slice( -2 );
+				}
+			}
+
+			// Add values to countdown
+			daysSelector.append( formatTime( time.days ) );
+			hoursSelector.append( formatTime( time.hours ) );
+			minutesSelector.append( formatTime( time.minutes ) );
+			secondsSelector.append( formatTime( time.seconds ) );			
+
+			// Update the clock values
+			function updateClock() {
+				// Get latest time
+				var time = getRemainingTime( end );
+
+				// Check if time values have changed 
+				if ( daysSelector.text() != time.days ) {
+					daysSelector.text( formatTime( time.days ) );
+				}
+				if ( hoursSelector.text() != time.hours ) {
+					hoursSelector.text( formatTime( time.hours ) );
+				}
+				if ( minutesSelector.text() != time.minutes ) {
+					minutesSelector.text( formatTime( time.minutes ) );
+				}
+				if ( secondsSelector.text() != time.seconds ) {
+					secondsSelector.text( formatTime( time.seconds ) );
+				}
+
+				// If there is no remaining time, stop the countdown
+				if ( time.remaining <= 0 ) {
+					clearInterval( timeinterval );
+				}
+			}
+
+			// Run update clock on one second intervals
+			updateClock();
+			var timeinterval = setInterval( updateClock, 1000 );
+
+			// Check if content is available for a message
+			if ( content ) {
+				// Shorten content 
+				var newContent = ( content.length > 100 ? ( content.substring( 0, 97 ) + '...' ) : content );
+
+				// Add message elements to countdown
+				countdown.append( '<div class="countdown-message"></div>' );
+
+				// Add new window attribute if input is "new"
+				if ( urlWindow && urlWindow.toLowerCase() == 'new' ) {
+					urlWindow = ' target="_blank"';
+				} else {
+					urlWindow = '';
+				}
+
+				// Add URL is there is one
+				if ( url && checkURL( url ) == true ) {
+					countdown.find( '.countdown-message' ).append( '<p><a href="' + url + '"' + urlWindow + '>' + newContent + '</a></p>' );
+				} else {
+					countdown.find( '.countdown-message' ).append( '<p>' + newContent + '</p>' );
+				}
+			}
+		} else {
+			countdown.remove();
+		}
+	});
+}
+
+// Hide countdown on home page is nothing is there
+function hideCountdown() {
+	if ( jQuery( '#countdown-promotion .wrapper' ).children().length == 0 ) {
+		jQuery( '#countdown-promotion' ).remove();
+	}
+}
+
+// Scroll to top functionality
+// Modified from https://paulund.co.uk/how-to-create-an-animated-scroll-to-top-with-jquery
+function scrollOnPage( selector, distance, position ) {
+	// Check to see if the window is top if not then display button
+	jQuery( window ).scroll( function() {
+		if ( jQuery( this ).scrollTop() > distance ) {
+			jQuery( selector ).fadeIn();
+		} else {
+			jQuery( selector ).fadeOut();
+		}
+	});
+	
+	// Click event to scroll to top
+	jQuery( selector ).click( function() {
+		jQuery( 'html, body' ).animate({
+			scrollTop : position
+		}, 1000 );
+		return false;
+	});	
+}
+
+// Spoiler toggle functionality
+function toggleSpoilerContent() {
+	// Loop through any spoiler codes on page
+	jQuery( '.spoiler' ).each( function() {
+		// Get variables for spoiler
+		var currentSpoiler = jQuery( this );
+		var spoilerClose = currentSpoiler.find( '.spoiler-close' );
+		var spoilerOpen = currentSpoiler.find( '.spoiler-open' );
+		var spoilerContent = currentSpoiler.find( '.spoiler-content' );
+
+		// Hide spoiler close tag and content by default
+		spoilerClose.add( spoilerContent ).addClass( 'spoiler-hide' );
+
+		// Click event for showing spoiler content
+		jQuery( spoilerOpen ).click( function() {
+			spoilerClose.add( spoilerContent ).addClass( 'spoiler-show' ).removeClass( 'spoiler-hide' );
+			spoilerOpen.addClass( 'spoiler-hide' ).removeClass( 'spoiler-show' );
+		});
+
+		// Click event for hiding spoiler content
+		jQuery( spoilerClose ).click( function() {
+			spoilerClose.add( spoilerContent ).addClass( 'spoiler-hide' ).removeClass( 'spoiler-show' );
+			spoilerOpen.addClass( 'spoiler-show' ).removeClass( 'spoiler-hide' );
+		});
+
+	});	
+}
+
+// Debounce function from underscore.js and https://davidwalsh.name/javascript-debounce-function
+function debounce( func, wait, immediate ) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if ( !immediate ) { 
+				func.apply( context, args );
+			}
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout( timeout );
+		timeout = setTimeout( later, wait );
+		if ( callNow ) { 
+			func.apply( context, args ); 
+		}
+	};
+};
+
+// Initialize Mobile Menu
+function initializeMobileMenu( options ) {
+	// Variables from mobile object
+	var menu = jQuery( options.menu );
+	var menuContainer = jQuery( options.menuContainer );
+	var mobileButton = jQuery( options.mobileButton );
+	var mobileMenu = jQuery( options.mobileMenu );
+	var width = options.width;
+
+	// Set a mobile false state (for window resize mainly)
+	var mobileOnce = false;
+
+	// Create open / close function
+	function toggleMobileMenu() {
+		if ( mobileMenu.hasClass( 'show' ) ) {
+			mobileMenu.removeClass( 'show' );
+			jQuery( 'body, html' ).removeClass( 'mobile-open' );
+		} else {
+			mobileMenu.addClass( 'show' );
+			jQuery( 'body, html' ).addClass( 'mobile-open' );
+		}		
+	}
+
+	// Add/remove classes when mobile menu button is clicked on
+	mobileButton.click( function() {
+		toggleMobileMenu();
+	});
+
+	// Resize actions for mobile menu
+	function mobileResizeAction() {
+		// Widths for em comparison		
+		var windowWidth = ( window.innerWidth / baseFontSize );
+		var docWidth = ( document.documentElement.clientWidth / baseFontSize );
+		var bodyWidth = ( document.body.clientWidth / baseFontSize );
+		var responseWidth = ( width / baseFontSize );
+
+		// Check all sorts of window and document widths to make sure resizing is consistent across browsers
+		if ( ( windowWidth || docWidth || bodyWidth ) <= responseWidth ) {			
+			// Check if mobile ones is false, meaning we haven't activated the mobile menu yet
+			if ( !mobileOnce ) {
+				// Create a mobile header and insert it at the top
+				var mobileButton = '<header class="mobile-menu-header"><span class="mobile-header">Menu</span><span class="icon icon-remove"></span></header>';
+				jQuery( mobileButton ).appendTo( mobileMenu );
+
+				// Close menu when button is clicked on
+				jQuery( '.mobile-menu-header .icon-remove' ).click( function() {
+					toggleMobileMenu();
+				});
+
+				// Move menu to menu container
+				menu.detach().appendTo( mobileMenu );
+
+				// In that mobile menu container, look for first level list and its items
+				menu.children( 'ul' ).children( 'li' ).each( function() {
+					// Get current selector and check if there are any second level lists
+					var current = jQuery( this );
+					var children = current.children( 'ul' );
+
+					// If there is a second level list, add a menu toggle icon
+					if ( children.length > 0 ) {
+						jQuery('<span class="icon icon-chevron-right slide-submenu"></span>').insertBefore( children );
+					}
+				});
+
+				// Add/remove classes to slide second level menu open
+				jQuery( '.slide-submenu' ).click( function() {
+					if ( jQuery( this ).next().hasClass( 'slide-open' ) ) {						
+						menu.children( 'ul' ).children( 'li' ).removeClass( 'slide-close' );
+						jQuery( this ).next().add( jQuery( this ).parent() ).removeClass( 'slide-open' );
+					} else {						
+						menu.children( 'ul' ).children( 'li' ).addClass( 'slide-close' );
+						jQuery( this ).next().add( jQuery( this ).parent() ).addClass( 'slide-open' ).removeClass('slide-close');
+					}
+				});	
+
+				// After everything has been done, set mobile to true so it's not run again on resize
+				mobileOnce = true;
+			}
+		} else {
+			// Check if mobile is true, meaning we're resizing and want to clean up on resize
+			if ( mobileOnce ) {
+				// Remove close button, replace menu, remove slide menu toggle, and remove any extra slide-open class
+				jQuery( '.mobile-menu-header' ).remove()
+				menu.detach().appendTo( menuContainer );
+				jQuery( '.slide-submenu' ).remove();
+				jQuery( 'ul, li' ).removeClass( 'slide-open' );
+				jQuery( 'li' ).removeClass( 'slide-close' );
+				jQuery( 'body, html' ).removeClass( 'mobile-open' );
+				mobileMenu.removeClass( 'show' );
+
+				// Then set mobile to false again so we can start over
+				mobileOnce = false;
+			}
+		}
+	}
+
+	// Call mobile menu once if browser is brought up or refreshed
+	mobileResizeAction();
+
+	// Then run mobile menu on resizing using debounce
+	var resizeForMobile = debounce(function() { 
+		mobileResizeAction(); 
+	}, 100 );
+	window.addEventListener( 'resize', resizeForMobile );
+}
---
+++ khyeras/_dev/styles/khyeras_v1/template/js/run-functions.js
@@ -0,0 +1,39 @@
+// Get distance for scroll to bottom
+var bottomDistance =  jQuery( document ).height() + jQuery( window ).height();
+
+// Base font size for responsive comparisons
+var baseFontSize = 16;
+
+// Run all functions on document ready
+jQuery( document ).ready( function( $ ) {
+    // toggleNavSubMenus( '#header-nav .menu-main-container .menu > li' );
+    // hideNavigation( 'body.attachment .navigation-links' );
+    // addFeatherLightGallery( '.gallery' );
+    // initializeCountdown();
+    // hideCountdown();
+    // scrollOnPage( '.scroll-to-top', 100, 0 );
+    // scrollOnPage( '.scroll-to-bottom', 100, bottomDistance );
+    // toggleSpoilerContent();
+
+    // initializeMobileMenu({
+    //     menu          : '#header-nav .menu-main-container',
+    //     menuContainer : '#header-nav .wrapper',
+    //     mobileButton  : '.mobile-menu-button',
+    //     mobileMenu    : '#mobile-menu',
+    //     width         : 768
+    // });
+
+    // $( '#front-page-sections #recent-reviews .insprvw-recent-reviews' ).owlCarousel({
+    // 	pagination     : false,
+    // 	navigation     : true,
+    // 	navigationText : [
+    // 		'<span class="icon icon-chevron-left"></span>',
+    // 		'<span class="icon icon-chevron-right"></span>'
+    // 	],
+    //     items : 5, //10 items above 1000px browser width
+    //     itemsDesktop : [1182,4], //5 items between 1000px and 901px
+    //     itemsDesktopSmall : [982,3], // betweem 900px and 601px
+    //     itemsTablet: [751,2], //2 items between 600 and 0
+    //     itemsMobile : [482,1] // itemsMobile disabled - inherit from itemsTablet option        
+    // });
+});
