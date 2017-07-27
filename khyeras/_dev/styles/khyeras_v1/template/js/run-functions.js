8c6844539cad4b65d790cdfb1b6e625a6ae02903
displaycoffee <adria.m.murphy@gmail.com>
2017-07-26 21:19:12 -0600

    Add class for 'no topics' forum, remove javascript, rename sass topiclist file

--- khyeras/_dev/styles/khyeras_v1/template/js/run-functions.js
+++ khyeras/_dev/styles/khyeras_v1/template/js/run-functions.js
@@ -6,34 +6,24 @@ var baseFontSize = 16;
 
 // Run all functions on document ready
 jQuery( document ).ready( function( $ ) {
-    // toggleNavSubMenus( '#header-nav .menu-main-container .menu > li' );
-    // hideNavigation( 'body.attachment .navigation-links' );
-    // addFeatherLightGallery( '.gallery' );
-    // initializeCountdown();
-    // hideCountdown();
-    // scrollOnPage( '.scroll-to-top', 100, 0 );
-    // scrollOnPage( '.scroll-to-bottom', 100, bottomDistance );
-    // toggleSpoilerContent();
+	// Check forum listing for no topics and add a class
+	var noTopics = $( '.section-viewforum .forum-title ~ .panel > .inner > strong' );
+	var noTopicsText = noTopics.text();
 
-    // initializeMobileMenu({
-    //     menu          : '#header-nav .menu-main-container',
-    //     menuContainer : '#header-nav .wrapper',
-    //     mobileButton  : '.mobile-menu-button',
-    //     mobileMenu    : '#mobile-menu',
-    //     width         : 768
-    // });
+	if ( noTopicsText == 'There are no topics or posts in this forum.' ) {
+		noTopics.closest( '.panel' ).addClass( 'no-topics' );
+	}
 
-    // $( '#front-page-sections #recent-reviews .insprvw-recent-reviews' ).owlCarousel({
-    // 	pagination     : false,
-    // 	navigation     : true,
-    // 	navigationText : [
-    // 		'<span class="icon icon-chevron-left"></span>',
-    // 		'<span class="icon icon-chevron-right"></span>'
-    // 	],
-    //     items : 5, //10 items above 1000px browser width
-    //     itemsDesktop : [1182,4], //5 items between 1000px and 901px
-    //     itemsDesktopSmall : [982,3], // betweem 900px and 601px
-    //     itemsTablet: [751,2], //2 items between 600 and 0
-    //     itemsMobile : [482,1] // itemsMobile disabled - inherit from itemsTablet option        
-    // });
+	// addFeatherLightGallery( '.gallery' );
+	// scrollOnPage( '.scroll-to-top', 100, 0 );
+	// scrollOnPage( '.scroll-to-bottom', 100, bottomDistance );
+	// toggleSpoilerContent();
+
+	// initializeMobileMenu({
+	//     menu          : '#header-nav .menu-main-container',
+	//     menuContainer : '#header-nav .wrapper',
+	//     mobileButton  : '.mobile-menu-button',
+	//     mobileMenu    : '#mobile-menu',
+	//     width         : 768
+	// });
 });
