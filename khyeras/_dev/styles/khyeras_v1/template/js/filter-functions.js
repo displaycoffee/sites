60681a80fc226bfc4589580042f47a68eb16f98c
displaycoffee <adria.m.murphy@gmail.com>
2020-04-09 18:09:04 -0600

    Work on filter functionality

--- khyeras/_dev/styles/khyeras_v1/template/js/filter-functions.js
+++ khyeras/_dev/styles/khyeras_v1/template/js/filter-functions.js
@@ -1,22 +1,10 @@
 function addFiltering() {
 	var filters = document.querySelectorAll( '.card-filters .card-filter-menu .card-filter' );
-	var blocks = document.querySelectorAll( '.card-block' );
 	var filtersActive = 'card-filter-active';
 	var toggleShow = 'toggle-show';
 	var toggleHide = 'toggle-hide';
 
-	if ( filters && filters.length && blocks && blocks.length ) {
-		// Get list of all cards on page that can be filtered
-		var cardBlocks = [];
-		for ( var h = 0; h < blocks.length; h++ ) {
-			var block = blocks[h];
-			cardBlocks.push({
-				'id'      : '#' + block.getAttribute( 'id' ),
-				'filters' : block.getAttribute( 'data-filters' ),
-				'applied' : 0
-			});
-		}
-
+	if ( filters && filters.length && cardBlocks ) {
 		// Check if there are already filter parameters applied to the url
 		if ( window.location.search ) {
 			var filterParameters = window.location.search.replace( '?', '' ).split( '&' );
@@ -51,6 +39,7 @@ function addFiltering() {
 				if ( filterParameters.indexOf( parameter ) !== -1 ) {
 					filterParameters.splice( filterParameters.indexOf( parameter ), 1 );
 					selector.classList.remove( filtersActive );
+					removeFilters( parameter );
 				} else {
 					// Otherwise add parameter and add active class
 					filterParameters.push( parameter );
@@ -65,37 +54,48 @@ function addFiltering() {
 
 		// Add classes to card blocks when filters are present
 		function applyFilters( parameter ) {
-			cardBlocks = cardBlocks.filter( function( block ) {
+			cardBlocks.filter( function( block ) {
 				if ( block.filters.indexOf( parameter ) !== -1 ) {
 					block.applied += 1;
 				}
-				return block;
-			});
 
-			// var blocks = document.querySelectorAll( '.card-block' );
-			//
-			// if ( blocks && blocks.length ) {
-			// 	for ( var k = 0; k < blocks.length; k++ ) {
-			// 		var block = blocks[k];
-			// 		var filterAttribute = block.getAttribute( 'data-filters' );
-			// 		var filterCount = ( block.getAttribute( 'data-applied-filters' ) * 1);
-			//
-			// 		if ( block.classList.contains( toggleShow ) ) {
-			// 			// do nothing if class contains toggle-show already
-			// 			// the block should still stay visible
-			// 		} else {
-			// 			if ( filterAttribute.indexOf( parameter ) !== -1 ) {
-			// 				if ( block.classList.contains( toggleHide ) ) {
-			// 					block.classList.remove( toggleHide );
-			// 				}
-			// 				block.classList.add( toggleShow );
-			// 				block.setAttribute( 'data-applied-filters', ( filterCount + 1 ) );
-			// 			} else {
-			// 				block.classList.add( toggleHide );
-			// 			}
-			// 		}
-			// 	}
-			// }
+				var blockSelector = document.querySelector( block.id );
+				if ( blockSelector.classList.contains( toggleShow ) ) {
+					// do nothing if class contains toggle-show already
+					// the block should still stay visible
+				} else {
+					if ( block.filters.indexOf( parameter ) !== -1 ) {
+						if ( blockSelector.classList.contains( toggleHide ) ) {
+							blockSelector.classList.remove( toggleHide );
+						}
+						blockSelector.classList.add( toggleShow );
+					} else {
+						blockSelector.classList.add( toggleHide );
+					}
+				}
+			});
+		}
+
+		// Add classes to card blocks when filters are present
+		function removeFilters( parameter ) {
+			cardBlocks.filter( function( block ) {
+				if ( block.filters.indexOf( parameter ) !== -1 ) {
+					block.applied -= 1;
+				}
+
+				var blockSelector = document.querySelector( block.id );
+				if ( blockSelector.classList.contains( toggleHide ) ) {
+					// do nothing if class contains toggle-hide already
+					// the block should stay hidden
+				} else {
+					if ( block.filters.indexOf( parameter ) !== -1 && block.applied < 1 ) {
+						if ( blockSelector.classList.contains( toggleShow ) ) {
+							blockSelector.classList.remove( toggleShow );
+						}
+						blockSelector.classList.add( toggleHide );
+					}
+				}
+			});
 		}
 	}
 }
